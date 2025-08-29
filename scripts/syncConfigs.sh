8c6599f59a6b9c52f0b73d6207b7039a9ad7da71
sunzhongyi <sunzhongyi@simboss.com>
2025-08-29 11:30:56 +0800

    chore: rename folder

--- scripts/syncConfigs.sh
+++ scripts/syncConfigs.sh
@@ -10,5 +10,5 @@ ln -sf $HOME/.dotfiles/.zshrc $HOME/.zshrc
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
 echo "Sync .config/clash"
 rm -rf $HOME/.config/clash
-ln -sf $HOME/.dotfiles/dot.config/clash $HOME/.config/clash
+ln -sf $HOME/.dotfiles/dot.configs/clash $HOME/.config/clash
 
